6a82850502bb4907b544ffb7fffb5763eed8dc20
Alan Khalili <khalilialan@gmail.com>
2023-06-04 12:41:58 -0700

    commented the code and deleted the unneccisarry 'remove limb' function

--- hangman.java
+++ hangman.java
@@ -3,35 +3,46 @@ import java.util.Scanner;
 class hangman {
 
     public static void main(String[] args) {
-        stats st = new stats();
-        String word;
+        stats st = new stats(); //Constructor for my stats.java class
+
+        String word; 
         boolean playing = true;
-        Scanner s = new Scanner(System.in);
+
+        Scanner s = new Scanner(System.in); //declared a scanner to get user input
+
         do {
-            st.reset();
+            st.reset(); //reset all values so they would not interupt the game.
 
             System.out.print("Welcome to hangman!\nPlayer 1, enter a word: ");
 
             word = s.next();
-            st.setWord(word);
+            st.setWord(word); //sent the word to the stats class
 
-            clearScreen();
+            clearScreen(); //put 25 blank lines
 
             do {
+                st.showStats(); //showed all of the stats for the game like the limbs remaining, lettersguessed, etc.
 
-                st.showStats();
                 System.out.print("PLayer 2, guess a letter: ");
-                st.p2guess(s.next().toLowerCase().charAt(0));
+                st.p2guess(s.next().toLowerCase().charAt(0)); //got input from player 2
+
                 clearScreen();
+
                 playing = st.gameOver();
+
             } while (playing);
+
         } while (playing);
-        s.close();
+
+        s.close(); //closed scanner
     }
 
     private static void clearScreen() {
+
         for (int i = 0; i < 25; i++) {
+
             System.out.println();
+            
         }
     }
 
--- stats.java
+++ stats.java
@@ -3,67 +3,105 @@ import java.util.ArrayList;
 public class stats {
     public int limbs = 10;
     public int lettersRemaining = 0;
+
     ArrayList<Character> wordArray = new ArrayList<>();
     ArrayList<Character> hiddenWordArray = new ArrayList<>();
     ArrayList<Character> lettersGuessed = new ArrayList<>();
 
-    public void showStats(){
+
+    public void showStats(){ //here is my first method it shows the stats as stated in hangman.java
+
         System.out.printf("Limbs remaining: %d%nLetters remaining: %d%nLetters guessed: ", limbs,
         lettersRemaining);
-        for(int i = 0; i < wordArray.size(); i++){
-            System.out.print(hiddenWordArray.get(i) + " ");
+
+        for(int i = 0; i < lettersGuessed.size(); i++){
+            
+            System.out.print(lettersGuessed.get(i) + " ");
+
         }
+
+        System.out.println();
+
+        for(int i = 0; i < wordArray.size(); i++){
+            
+            System.out.print(hiddenWordArray.get(i) + " ");
+
+        }
+
         System.out.println();
     }
 
-    public void p2guess(Character a) {
+
+    public void p2guess(Character a) { //here is my second method. It takes player 2's guess and sees if it was right, wrong, or duplicate.
+
         if(wordArray.contains(a)){
+            
             do{
+
                 --lettersRemaining;
+
                 hiddenWordArray.set(wordArray.indexOf(a), a);
                 wordArray.set(wordArray.indexOf(a), '-');
 
             }while(wordArray.contains(a));
+
             lettersGuessed.add(a);
         }
+
         else if(lettersGuessed.contains(a)){
+
             System.out.println("You already guessed that letter.");
+            
         }
+
         else{
+
             --limbs;
             lettersGuessed.add(a);
+
         }
     }
 
-    public void removeLimb() {
-        limbs -= 1;
-    }
 
-    public void setWord(String a) {
+    public void setWord(String a) { //here is my third method. It gets the word from the scanner and sets adds it to the arraylists.
+
         lettersRemaining = a.length();
+
         for (int i = 0; i < a.length(); i++) {
+
             wordArray.add(a.toLowerCase().charAt(i));
             hiddenWordArray.add('-');
+
         }
     }
 
-    public void reset() {
+    
+    public void reset() { //here it just resets the info.
         limbs = 10;
         lettersGuessed.clear();
         wordArray.clear();
         hiddenWordArray.clear();
     }
 
-    public boolean gameOver() {
+    public boolean gameOver() { //here is my last method in stats.java. it just checks the info to see if the game is over or not.
+
         if (limbs == 0) {
+
             System.out.println("Player 1 Wins!");
             return false;
+
         }
+
         if (lettersRemaining == 0) {
+
             System.out.println("Player 2 Wins!");
             return false;
-        } else {
+        } 
+        
+        else {
+
             return true;
+
         }
     }
 }
